a2e478a8b68e8c9543c86bcc0fd92a59bc474a6b
蔡俊贤 <1203673288@qq.com>
2023-03-20 15:27:58 +0800

    Logical Test 1

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -19,3 +19,37 @@ if (! function_exists('settings')) {
         return app('anlutro\LaravelSettings\SettingStore')->get($key, $default);
     }
 }
+
+if (! function_exists('check_stock_location')) {
+
+    /**
+     * Find missing location.
+     *
+     * @param string $location_str stock location separated by ","
+     * @return string missing location if exists or return "All stock location is used."
+     */
+    function check_stock_location(string $location_str) {
+        $location_arr = explode(',', $location_str);
+
+        $location_key_suffix = [];
+        foreach ($location_arr as $location) {
+            $location_key_suffix[substr($location, 0, -1)][] =  substr($location, -1, 1);
+        }
+
+        $start = ord('A');
+        $return_str = '';
+        foreach ($location_key_suffix as $key => $suffix) {
+            rsort($suffix);
+            $end = ord($suffix[0]);
+            if ($end > $start) {
+                $diff_location = array_diff(range('A', $suffix[0]), $suffix);
+                $diff_location && $return_str .= 'Missing location ' . $key . implode(',', $diff_location) . ";\n";
+            }
+        }
+
+        if (!$return_str) {
+            return 'All stock location is used.';
+        }
+        return $return_str;
+    }
+}
